fa0141e1641905bbcb2f783a6816ebda96c9c6d8
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2010-09-23 15:00:30 +0000

    HH-15492 SingleConnectionFactory simplified down to single connection
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@64432 9079aaf0-2f0c-0410-9ff6-b2184b462109

--- src/main/java/ru/hh/rabbitmq/impl/SingleConnectionFactory.java
+++ src/main/java/ru/hh/rabbitmq/impl/SingleConnectionFactory.java
@@ -2,109 +2,64 @@ package ru.hh.rabbitmq.impl;
 
 import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionParameters;
 import com.rabbitmq.client.ShutdownListener;
 import com.rabbitmq.client.ShutdownSignalException;
 import java.io.IOException;
-import javax.net.SocketFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.hh.rabbitmq.ConnectionFactory;
-import ru.hh.rabbitmq.NotInitializedException;
 
 public class SingleConnectionFactory implements ConnectionFactory, ShutdownListener {
   private static final Logger logger = LoggerFactory.getLogger(SingleConnectionFactory.class);
 
-  private ConnectionParameters connectionParameters;
-  private Address[] addresses;
-  private SocketFactory socketFactory;
-  private Integer closeTimeout;
-
-  private com.rabbitmq.client.ConnectionFactory connectionFactory;
-  private volatile boolean initialized;
+  private final Address[] addresses;
+  private final com.rabbitmq.client.ConnectionFactory connectionFactory;
+  
+  // guarded by synchronized(this)
+  private Connection connection;
+  
   private volatile boolean shuttingDown;
 
-  public SingleConnectionFactory(ConnectionParameters connectionParameters, Address... addresses) {
-    this.connectionParameters = connectionParameters;
+  public SingleConnectionFactory(com.rabbitmq.client.ConnectionFactory connectionFactory, Address... addresses) {
+    this.connectionFactory = connectionFactory;
     this.addresses = addresses;
   }
 
-  public SingleConnectionFactory(ConnectionParameters connectionParameters, SocketFactory socketFactory, Address... addresses) {
-    this.connectionParameters = connectionParameters;
-    this.addresses = addresses;
-    this.socketFactory = socketFactory;
-  }
-
-  public SingleConnectionFactory(
-      ConnectionParameters connectionParameters, SocketFactory socketFactory, Integer closeTimeout, Address... addresses) {
-    this.connectionParameters = connectionParameters;
-    this.addresses = addresses;
-    this.socketFactory = socketFactory;
-    this.closeTimeout = closeTimeout;
-  }
-
-  public void init() {
-    logger.debug("Started initializing");
-    if (initialized) {
-      throw new IllegalStateException("Already initialized!");
+  public synchronized Connection openConnection() throws IOException {
+    logger.debug("Opening new connection");
+    if (shuttingDown) {
+      throw new IllegalStateException("Shutting down");
     }
-
-    connectionFactory = new com.rabbitmq.client.ConnectionFactory();
-    if (socketFactory != null) {
-      connectionFactory.setSocketFactory(socketFactory);
+    if (connection == null) {
+      connection = connectionFactory.newConnection(addresses);
+      connection.addShutdownListener(this);
     }
-    connectionFactory.setUsername(connectionParameters.getUserName());
-    connectionFactory.setPassword(connectionParameters.getPassword());
-    connectionFactory.setVirtualHost(connectionParameters.getVirtualHost());
-    connectionFactory.setRequestedChannelMax(connectionParameters.getRequestedChannelMax());
-    connectionFactory.setRequestedFrameMax(connectionParameters.getRequestedFrameMax());
-    connectionFactory.setRequestedHeartbeat(connectionParameters.getRequestedHeartbeat());
-    initialized = true;
-    logger.debug("Finished initializing");
-  }
-
-  public Connection openConnection() throws IOException {
-    logger.debug("Openning new connection");
-    ensureRunning();
-    Connection connection = connectionFactory.newConnection(addresses);
-    connection.addShutdownListener(this);
+    // TODO check connection availability
     return connection;
   }
 
   public void returnConnection(Connection connection) {
-    logger.debug("Closing connection");
+    // nothing to do here
+  }
+
+  @Override
+  public synchronized void close() {
+    logger.debug("Closing factory");
+    
+    shuttingDown = true;
+    
     if (connection == null) {
       return;
     }
-
     if (!connection.isOpen()) {
       logger.warn("Connection is already closing, ignoring");
       return;
     }
-
     try {
-      if (closeTimeout != null) {
-        connection.close(closeTimeout);
-        return;
-      }
+      logger.debug("Closing connection");
       connection.close();
     } catch (IOException e) {
       logger.warn("Error while closing connection, ignoring", e);
-    }
-  }
-
-  @Override
-  public void close() {
-    logger.debug("Closing");
-    shuttingDown = true;
-  }
-
-  private void ensureRunning() {
-    if (!initialized) {
-      throw new NotInitializedException();
-    }
-    if (shuttingDown) {
-      throw new IllegalStateException("Shutting down");
     }
   }
 
--- src/test/java/ru/hh/rabbitmq/impl/ConnectionFactoryImplTest.java
+++ src/test/java/ru/hh/rabbitmq/impl/ConnectionFactoryImplTest.java
@@ -4,23 +4,17 @@ import com.headhunter.test.Mocks;
 import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.ConnectionParameters;
 import java.io.IOException;
-import javax.net.SocketFactory;
 import junit.framework.Assert;
 import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
-import ru.hh.rabbitmq.BeanUtils;
 
 public class ConnectionFactoryImplTest {
   private static final String HOST = "localhost";
   private static final int PORT = 1;
-  private static final int CLOSE_TIMEOUT = 1;
 
-  private SocketFactory socketFactory;
   private SingleConnectionFactory impl;
-  private ConnectionParameters params;
   private ConnectionFactory connectionFactory;
   private Address[] addresses;
 
@@ -29,37 +23,19 @@ public class ConnectionFactoryImplTest {
   @Before
   public void setUp() throws Exception {
     mm = new Mocks();
-    socketFactory = mm.createMock(SocketFactory.class);
-    params = new ConnectionParameters();
     addresses = new Address[] { new Address(HOST, PORT) };
-
-    impl = new SingleConnectionFactory(params, socketFactory, CLOSE_TIMEOUT, addresses);
-    impl.init();
-
     connectionFactory = mm.createMock(ConnectionFactory.class);
-
-    BeanUtils.setField(impl, "connectionFactory", connectionFactory);
+    impl = new SingleConnectionFactory(connectionFactory, addresses);
   }
 
   @Test
   public void testOpenConnection() throws IOException {
-    testOpenConnectionInternal();
-
-    impl.close();
-
-    try {
-      testOpenConnectionInternal();
-      Assert.fail();
-    } catch (IllegalStateException e) { }
-  }
-
-  private void testOpenConnectionInternal() throws IOException {
-    Connection connection = EasyMock.createMock(Connection.class);
+    Connection connection = mm.createMock(Connection.class);
     connectionFactory.newConnection(EasyMock.eq(addresses));
-    EasyMock.expectLastCall().andReturn(connection).anyTimes();
+    mm.expectLastCall().andReturn(connection).anyTimes();
 
     connection.addShutdownListener(impl);
-    EasyMock.expectLastCall().anyTimes();
+    mm.expectLastCall().anyTimes();
 
     mm.replay();
 
@@ -67,19 +43,20 @@ public class ConnectionFactoryImplTest {
 
     mm.verify();
     mm.reset();
-  }
-
-  public void testReturnConnection() throws IOException {
-    Connection connection = EasyMock.createMock(Connection.class);
+    
+    connection.close();
+    mm.expectLastCall().once();
     connection.isOpen();
     mm.expectLastCall().andReturn(true).anyTimes();
-    connection.close(EasyMock.eq(CLOSE_TIMEOUT));
-    mm.expectLastCall().anyTimes();
-
     mm.replay();
-
-    impl.returnConnection(connection);
-
+    
+    impl.close();
+    
     mm.verify();
+    
+    try {
+      impl.openConnection();
+      Assert.fail();
+    } catch (IllegalStateException e) { }
   }
 }
