23177bab6922aa3bde09ee75b590fe222f6fc7be
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2010-09-23 15:00:45 +0000

    HH-15492 ChannelFactoryImpl test fixed
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@64446 9079aaf0-2f0c-0410-9ff6-b2184b462109

--- src/test/java/ru/hh/rabbitmq/impl/ChannelFactoryImplTest.java
+++ src/test/java/ru/hh/rabbitmq/impl/ChannelFactoryImplTest.java
@@ -4,16 +4,12 @@ import com.headhunter.test.Mocks;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import java.io.IOException;
-import junit.framework.Assert;
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 import ru.hh.rabbitmq.ConnectionFactory;
 
 public class ChannelFactoryImplTest {
   private static final int QUEUE_QOS = 1;
-  private static final boolean QUEUE_DURABLE = true;
-  private static final String QUEUE_NAME = "one";
   private Mocks mm;
   private ConnectionFactory factory;
   private ChannelFactoryImpl impl;
@@ -27,40 +23,6 @@ public class ChannelFactoryImplTest {
 
   @Test
   public void testOpenChannel() throws IOException {
-    mockOpenChannel();
-    mm.replay();
-    impl.getChannel();
-    mm.verify();
-  }
-
-  @Test
-  public void testOpenAfterClose() throws IOException {
-    mockClose(null);
-    mm.replay();
-    impl.close();
-    mm.verify();
-    try {
-      impl.getChannel();
-      Assert.fail();
-    } catch (IllegalStateException e) { }
-  }
-
-  @Test
-  public void testReturnChannel() throws IOException {
-    Channel channel = mm.createMock(Channel.class);
-
-    channel.isOpen();
-    mm.expectLastCall().andReturn(true).anyTimes();
-
-    channel.close();
-    mm.expectLastCall().anyTimes();
-
-    mm.replay();
-    impl.returnChannel(channel);
-    mm.verify();
-  }
-
-  private void mockOpenChannel() throws IOException {
     Connection connection = mm.createMock(Connection.class);
 
     connection.isOpen();
@@ -75,15 +37,31 @@ public class ChannelFactoryImplTest {
 
     channel.basicQos(QUEUE_QOS);
     mm.expectLastCall().anyTimes();
+    mm.replay();
+    impl.getChannel();
+    mm.verify();
   }
 
-  private void mockClose(Connection connection) {
-    factory.returnConnection(EasyMock.eq(connection));
-    mm.expectLastCall().anyTimes();
+  @Test
+  public void testReturnChannel() throws IOException {
+    Channel channel = mm.createMock(Channel.class);
+    Connection connection = mm.createMock(Connection.class);
+    
+    channel.isOpen();
+    mm.expectLastCall().andReturn(true).anyTimes();
+    
+    channel.getConnection();
+    mm.expectLastCall().andReturn(connection).anyTimes();
 
-    if (connection != null) {
-      connection.isOpen();
-      mm.expectLastCall().andReturn(false).anyTimes();
-    }
+    channel.close();
+    mm.expectLastCall().once();
+    
+    factory.returnConnection(connection);
+    mm.expectLastCall().once();
+      
+    mm.replay();
+    impl.returnChannel(channel);
+    mm.verify();
   }
+
 }
