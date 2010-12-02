dd6a2a50750f1c7b59167e17aa5df8daa872d15b
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2010-12-02 11:25:49 +0000

    HH-16951 ChannelTaskFuture extracted
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@68101 9079aaf0-2f0c-0410-9ff6-b2184b462109

---
+++ src/main/java/ru/hh/rabbitmq/send/ChannelTaskFuture.java
@@ -0,0 +1,24 @@
+package ru.hh.rabbitmq.send;
+
+import com.google.common.util.concurrent.AbstractFuture;
+import com.rabbitmq.client.Channel;
+
+class ChannelTaskFuture extends AbstractFuture<Void> implements ChannelTask {
+  private final ChannelTask task;
+
+  ChannelTaskFuture(ChannelTask task) {
+    this.task = task;
+  }
+
+  @Override
+  public void run(Channel channel) {
+    try {
+      if (!isCancelled()) {
+        task.run(channel);
+        set(null);
+      }
+    } catch (RuntimeException e) {
+      setException(e);
+    }
+  }
+}
--- src/main/java/ru/hh/rabbitmq/send/Publisher.java
+++ src/main/java/ru/hh/rabbitmq/send/Publisher.java
@@ -1,44 +1,16 @@
 package ru.hh.rabbitmq.send;
 
 import com.google.common.base.Service;
-import com.google.common.util.concurrent.AbstractExecutionThreadService;
-import com.google.common.util.concurrent.AbstractFuture;
-import com.rabbitmq.client.Channel;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.hh.rabbitmq.ChannelFactory;
 
 public class Publisher {
   private static final Logger logger = LoggerFactory.getLogger(Publisher.class);
   
   private final Service[] workers;
   private final BlockingQueue<ChannelTask> taskQueue;
-
-  private static class ChannelTaskFuture extends AbstractFuture<Void> implements ChannelTask {
-    private final ChannelTask task;
-
-    private ChannelTaskFuture(ChannelTask task) {
-      this.task = task;
-    }
-
-    @Override
-    public void run(Channel channel) {
-      try {
-        if (!isCancelled()) {
-          task.run(channel);
-          set(null);
-        }
-      } catch (RuntimeException e) {
-        setException(e);
-      }
-    }
-  }
 
   private Future<Void> submit(final ChannelTask task) {
     ChannelTaskFuture future = new ChannelTaskFuture(task);
