68ec54a3e2201bbe2ee1b7a742190934b253080d
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2010-12-02 11:25:50 +0000

    HH-16951 publisher construction and startup
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@68104 9079aaf0-2f0c-0410-9ff6-b2184b462109

--- src/main/java/ru/hh/rabbitmq/send/Publisher.java
+++ src/main/java/ru/hh/rabbitmq/send/Publisher.java
@@ -1,16 +1,45 @@
 package ru.hh.rabbitmq.send;
 
 import com.google.common.base.Service;
+import com.rabbitmq.client.Address;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.hh.rabbitmq.ConnectionFactory;
+import ru.hh.rabbitmq.impl.ChannelFactoryImpl;
+import ru.hh.rabbitmq.impl.SingleConnectionFactory;
+import ru.hh.rabbitmq.util.Addresses;
 
 public class Publisher {
   private static final Logger logger = LoggerFactory.getLogger(Publisher.class);
   
+  private final ConnectionFactory[] connectionFactories;
   private final Service[] workers;
   private final BlockingQueue<ChannelTask> taskQueue;
+
+
+  public Publisher(com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay,
+                   int attempts, int queueLength, Address... addresses) {
+    if (addresses.length < 1) {
+      throw new IllegalArgumentException("can't create Publisher without connection addresses");
+    }
+    connectionFactories = new ConnectionFactory[addresses.length];
+    workers = new Service[addresses.length];
+    taskQueue = new ArrayBlockingQueue<ChannelTask>(queueLength);
+    for(int i = 0; i < addresses.length; i ++) {
+      connectionFactories[i] = new SingleConnectionFactory(connectionFactory, retryUnit, retryDelay, attempts, addresses[i]);
+      workers[i] = new ChannelWorker(new ChannelFactoryImpl(connectionFactories[i]), taskQueue, addresses[i].toString() + "-publisher-worker");
+      workers[i].start();
+    }
+  }
+
+  public Publisher(com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay,
+                   int attempts, int queueLength, String hosts, int port) {
+    this(connectionFactory, retryUnit, retryDelay, attempts, queueLength, Addresses.split(hosts, port));
+  }
 
   private Future<Void> submit(final ChannelTask task) {
     ChannelTaskFuture future = new ChannelTaskFuture(task);
@@ -22,6 +51,8 @@ public class Publisher {
     for (Service worker : workers) {
       worker.stopAndWait();
     }
+    for(ConnectionFactory factory : connectionFactories) {
+      factory.close();
+    }
   }
-
 }
