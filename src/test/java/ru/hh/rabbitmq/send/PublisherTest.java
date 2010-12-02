2d193c5bbaab4578fed1c1ee8b501767e5c32624
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2010-12-02 11:26:10 +0000

    HH-16951 wait for send operations for complete
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@68140 9079aaf0-2f0c-0410-9ff6-b2184b462109

--- src/test/java/ru/hh/rabbitmq/send/PublisherTest.java
+++ src/test/java/ru/hh/rabbitmq/send/PublisherTest.java
@@ -3,15 +3,19 @@ package ru.hh.rabbitmq.send;
 import com.headhunter.test.Mocks;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.junit.Test;
 import ru.hh.rabbitmq.simple.BodilessMessage;
 import ru.hh.rabbitmq.simple.Message;
 
 public class PublisherTest {
   @Test
-  public void send() {
+  public void send() throws ExecutionException, InterruptedException, TimeoutException {
     Mocks mocks = new Mocks();
     com.rabbitmq.client.ConnectionFactory connectionFactory = mocks.createMock(com.rabbitmq.client.ConnectionFactory.class);
     mocks.replay();
@@ -19,8 +23,12 @@ public class PublisherTest {
     Destination destination = new Destination("", "trash", true, false);
     Message message = new BodilessMessage(new HashMap<String, Object>());
     List<Message> messages = Arrays.asList(message, message, message);
+    List<Future<Void>> futures = new LinkedList<Future<Void>>();
     for (int i = 0; i < 3; i++) {
-      publisher.send(destination, messages);
+      futures.add(publisher.send(destination, messages));
+    }
+    for (Future<Void> future : futures) {
+      future.get(100, TimeUnit.MILLISECONDS);
     }
     publisher.close();
     mocks.verify();
