3db6da61a69c34088e3497b70cff071fab24fb90
a.lebedevich <a.lebedevich@9079aaf0-2f0c-0410-9ff6-b2184b462109>
2011-01-24 15:33:48 +0000

    use only one connection thread in test
    git-svn-id: https://svn.hh.ru/headhunter.ru/commons/rabbitmq-client/trunk@71205 9079aaf0-2f0c-0410-9ff6-b2184b462109

--- src/test/java/ru/hh/rabbitmq/send/PublisherTest.java
+++ src/test/java/ru/hh/rabbitmq/send/PublisherTest.java
@@ -31,10 +31,9 @@ public class PublisherTest {
     
     com.rabbitmq.client.ConnectionFactory connectionFactory = mocks.createMock(com.rabbitmq.client.ConnectionFactory.class);
     mockConnectionTo(new Address("localhost", 5672), connectionFactory, mocks);
-    mockConnectionTo(new Address("anotherhost", 5672), connectionFactory, mocks);
     
     mocks.replay();
-    Publisher publisher = new Publisher(connectionFactory, TimeUnit.SECONDS, 1, 1, 3, "localhost, anotherhost", 5672);
+    Publisher publisher = new Publisher(connectionFactory, TimeUnit.SECONDS, 1, 1, 3, "localhost", 5672);
     Destination destination = new Destination("", "trash", true, false);
     Message message = new BodilessMessage(new HashMap<String, Object>());
     List<Message> messages = Arrays.asList(message, message, message);
