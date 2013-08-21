834aaf0e22ea93e81ccd6c6b0013bf9b607a2a94
Timofey <t.petrov@hh.by>
2013-08-21 19:07:30 +0400

    HH-36429 add logging of connection and message send time when inner queue is full

--- src/main/java/ru/hh/rabbitmq/send/Publisher.java
+++ src/main/java/ru/hh/rabbitmq/send/Publisher.java
@@ -3,8 +3,10 @@ package ru.hh.rabbitmq.send;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.Service;
 import com.rabbitmq.client.Address;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -18,43 +20,102 @@ import ru.hh.rabbitmq.simple.Message;
 import ru.hh.rabbitmq.util.Addresses;
 
 public class Publisher {
+
+  private static final long DEFAULT_RETRY_DELAY = 100;
+  private static final int DEFAULT_ATTEMPTS_NUMBER = 3;
+  private static final int DEFAULT_INNER_QUEUE_CAPACITY = 1000;
+
   public static final Logger logger = LoggerFactory.getLogger(Publisher.class);
 
-  private final ConnectionFactory[] connectionFactories;
-  private final Service[] workers;
-  private final BlockingQueue<PublishTaskFuture> taskQueue;
+  private com.rabbitmq.client.ConnectionFactory connectionFactory;
+  private List<Address> addresses = new ArrayList<Address>();
+  private long retryDelay = DEFAULT_RETRY_DELAY;
+  private TimeUnit retryDelayUnits = TimeUnit.MILLISECONDS;
+  private int attemptsNumber = DEFAULT_ATTEMPTS_NUMBER;
+  private int innerQueueCapacity = DEFAULT_INNER_QUEUE_CAPACITY;
 
-  public Publisher(
-      com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay, int attempts,
-      int maxQueueLength, Address... addresses) {
-    if (addresses.length < 1) {
-      throw new IllegalArgumentException("no connection addresses");
-    }
-    connectionFactories = new ConnectionFactory[addresses.length];
-    workers = new Service[addresses.length];
-    taskQueue = new ArrayBlockingQueue<PublishTaskFuture>(maxQueueLength);
-    for (int i = 0; i < addresses.length; i++) {
-      connectionFactories[i] = new SingleConnectionFactory(connectionFactory, retryUnit, retryDelay, attempts, addresses[i]);
-      workers[i] =
-        new ChannelWorker(
-          new ChannelFactoryImpl(connectionFactories[i]), taskQueue, addresses[i].toString() + "-publisher-worker");
-      workers[i].start();
-    }
+  private Long connectTimeTolerance;
+  private Long sendTimeTolerance;
+
+  private final List<ConnectionFactory> factories = new ArrayList<ConnectionFactory>();
+  private final List<Service> workers = new ArrayList<Service>();
+
+  private BlockingQueue<PublishTaskFuture> taskQueue;
+
+  public Publisher(com.rabbitmq.client.ConnectionFactory connectionFactory, String hosts, int port) {
+    this(connectionFactory, Addresses.split(hosts, port));
   }
 
-  public Publisher(
-      com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay, int attempts, int queueLength,
-      String hosts, int port) {
-    this(connectionFactory, retryUnit, retryDelay, attempts, queueLength, Addresses.split(hosts, port));
+  public Publisher(com.rabbitmq.client.ConnectionFactory connectionFactory, Address... addresses) {
+    if (addresses == null || addresses.length == 0) {
+      throw new IllegalArgumentException("no connection addresses");
+    }
+    this.connectionFactory = connectionFactory;
+    this.addresses.addAll(Arrays.asList(addresses));
+  }
+
+  public void setRetryDelay(long retryDelay) {
+    this.retryDelay = retryDelay;
+  }
+
+  public void setRetryDelayUnits(TimeUnit retryDelayUnits) {
+    this.retryDelayUnits = retryDelayUnits;
+  }
+
+  public void setAttemptsNumber(int attemptsNumber) {
+    this.attemptsNumber = attemptsNumber;
+  }
+
+  public void setInnerQueueCapacity(int innerQueueCapacity) {
+    this.innerQueueCapacity = innerQueueCapacity;
+  }
+
+  public void setConnectTimeTolerance(Long connectTimeTolerance) {
+    this.connectTimeTolerance = connectTimeTolerance;
+  }
+
+  public void setSendTimeTolerance(Long sendTimeTolerance) {
+    this.sendTimeTolerance = sendTimeTolerance;
+  }
+
+  public void start() {
+    taskQueue = new ArrayBlockingQueue<PublishTaskFuture>(innerQueueCapacity);
+    for(Address address : addresses) {
+      ConnectionFactory factory = new SingleConnectionFactory(connectionFactory, retryDelayUnits, retryDelay, attemptsNumber, address);
+      factories.add(factory);
+      Service worker = new ChannelWorker(new ChannelFactoryImpl(factory), taskQueue, address.toString() + "-publisher-worker",
+              connectTimeTolerance, sendTimeTolerance);
+      workers.add(worker);
+      worker.start();
+    }
   }
 
   public void close() {
     for (Service worker : workers) {
       worker.stopAndWait();
     }
-    for (ConnectionFactory factory : connectionFactories) {
+    for (ConnectionFactory factory : factories) {
       factory.close();
     }
+  }
+
+  @Deprecated
+  public Publisher(
+      com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay, int attempts,
+      int maxQueueLength, Address... addresses) {
+    this(connectionFactory, addresses);
+    setRetryDelay(retryDelay);
+    setRetryDelayUnits(retryUnit);
+    setAttemptsNumber(attempts);
+    setInnerQueueCapacity(maxQueueLength);
+    start();
+  }
+
+  @Deprecated
+  public Publisher(
+      com.rabbitmq.client.ConnectionFactory connectionFactory, TimeUnit retryUnit, long retryDelay, int attempts, int queueLength,
+      String hosts, int port) {
+    this(connectionFactory, retryUnit, retryDelay, attempts, queueLength, Addresses.split(hosts, port));
   }
 
   /**
