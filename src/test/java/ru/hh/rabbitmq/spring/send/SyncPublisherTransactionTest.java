388d7dc2b352067d3c745fc45580f5c24a397794
Nick Voznesenskiy <voznesenskiy@hh.ru>
2016-10-21 13:05:47 +0300

    HH-63030 Add note regarding missing test method

--- src/test/java/ru/hh/rabbitmq/spring/send/SyncPublisherTransactionTest.java
+++ src/test/java/ru/hh/rabbitmq/spring/send/SyncPublisherTransactionTest.java
@@ -3,6 +3,7 @@ package ru.hh.rabbitmq.spring.send;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Test;
@@ -17,10 +18,12 @@ public class SyncPublisherTransactionTest extends SyncRabbitIntegrationTestBase 
 
   @Test
   public void testTransaction() throws InterruptedException {
-    final SyncPublisher publisher = publisher(HOST1, true).withJsonMessageConverter().build();
+    final SyncPublisher publisher = publisher(HOST1, true).withJsonMessageConverter().setTransactional(true).build();
     RabbitTransactionManager transactionManager = new RabbitTransactionManager(publisher.getTemplate().getConnectionFactory());
     TransactionTemplate transaction = new TransactionTemplate(transactionManager);
     publisher.startAsync();
+
+    assertTrue(publisher.getTemplate().isChannelTransacted());
 
     MessageHandler handler = new MessageHandler();
     Receiver receiver = receiverAllHosts(false).withJsonListener(handler).forQueues(QUEUE1).start();
@@ -46,17 +49,18 @@ public class SyncPublisherTransactionTest extends SyncRabbitIntegrationTestBase 
 
   @Test
   public void testTransactionWithRollback() throws InterruptedException {
-    final SyncPublisher publisher = publisher(HOST1, true).withJsonMessageConverter().build();
+    final SyncPublisher publisher = publisher(HOST1, true).withJsonMessageConverter().setTransactional(true).build();
     RabbitTransactionManager transactionManager = new RabbitTransactionManager(publisher.getTemplate().getConnectionFactory());
     TransactionTemplate transaction = new TransactionTemplate(transactionManager);
     publisher.startAsync();
+
+    assertTrue(publisher.getTemplate().isChannelTransacted());
 
     MessageHandler handler = new MessageHandler();
     Receiver receiver = receiverAllHosts(false).withJsonListener(handler).forQueues(QUEUE1).start();
 
     final Map<String, Object> sentMessage = new HashMap<>();
     sentMessage.put("data", QUEUE1);
-
 
     try {
       transaction.execute(new TransactionCallback<Void>() {
@@ -70,12 +74,18 @@ public class SyncPublisherTransactionTest extends SyncRabbitIntegrationTestBase 
     catch (PlannedException e) {
     }
 
-    Map<String, Object> receivedMessage  = handler.get();
+    Map<String, Object> receivedMessage = handler.get();
     assertNull(receivedMessage);
 
     publisher.stopSync();
     receiver.shutdown();
   }
+
+  // XXX there should be also testTransactionDisabledWithRollback() method to test that builder.setTransactional(false) will forbid
+  // transaction even if running in transaction context, but RabbitTransactionManager ignores that setting on RabbitTemplate and
+  // enforces tx anyway.
+  // It is not how regular transactions (i.e. in hh.ru) work - usually that setting is propagated and processed properly.
+  // This was manually tested in hh.ru environment.
 
   private static class PlannedException extends RuntimeException {
   }
